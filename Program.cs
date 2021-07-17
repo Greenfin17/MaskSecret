fbfaf230087106f2972f1203a80ce7aa732e84ec
John Maple <jmaple@bellsouth.net>
2021-07-17 12:44:35 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MaskSecret
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int strLength = 0;
+            Console.WriteLine("Enter secret string");
+            string secret = Console.ReadLine();
+            strLength = secret.Length;
+            Console.WriteLine("Your secret:");
+            {
+                for(int i = 0; i < strLength - 4; i++)
+                {
+                    Console.Write('*');
+                }
+            }
+            Console.Write($"{secret.Substring(strLength - 4)}");
+
+            Console.WriteLine("\nEnter any key to exit");
+            var key = Console.ReadKey();
+        }
+    }
+}
